14c6cd0e4f5ce49c307e317e73f007076b1488d1
agusrs <39487421+agusrs@users.noreply.github.com>
2020-10-07 20:12:17 -0300

    Revert "Added context"
    This reverts commit cdf0d3bacfd0c88795f892312286dcd45f16ddba.

--- src/App.js
+++ src/App.js
@@ -5,22 +5,19 @@ import Header from './components/header/Header';
 import Home from './components/home/Home';
 import Monitoreo from './components/monitoreo/Monitoreo';
 import Recoleccion from './components/recoleccion/Recoleccion';
-import {StoreProvider} from './context/Store';
 
 function App() {
   return (
-    <StoreProvider>
-      <Router>
-        <div className="App">
-          <Header/>
-          <Switch>
-            <Route path="/" exact component={Home} />
-            <Route path="/monitoreo" exact component={Monitoreo} />
-            <Route path="/recoleccion" exact component={Recoleccion} />
-          </Switch>
-        </div>
-      </Router>
-    </StoreProvider>
+    <Router>
+      <div className="App">
+        <Header/>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/monitoreo" exact component={Monitoreo} />
+          <Route path="/recoleccion" exact component={Recoleccion} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
